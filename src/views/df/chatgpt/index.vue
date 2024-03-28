0c36ee9528ce90df5d090b51977b83b73bf9a2c5
gfish007 <dfish0712@outlook.com>
2024-03-28 10:58:04 +0800

    rollback

--- src/views/df/chatgpt/index.vue
+++ src/views/df/chatgpt/index.vue
@@ -1,18 +1,14 @@
 <template>
-<iframe
-      src="http://192.168.3.10:1002/"
-      :style="{ height: 'calc(100vh - 53px)', width: '100%' }"
-      sandbox="allow-same-origin allow-scripts"
-    >
-    </iframe>
+  <ChatgptMobile v-if="getIsMobile" />
+  <ChatgptPc v-else />
 </template>
 
 <script lang="ts" setup>
-  import ChatgptMobile from './chatgpt_mobile.vue';
-  import ChatgptPc from './chatgpt_pc.vue';
-  import { useAppInject } from '/@/hooks/web/useAppInject';
+import ChatgptMobile from './chatgpt_mobile.vue';
+import ChatgptPc from './chatgpt_pc.vue';
+import { useAppInject } from '/@/hooks/web/useAppInject';
 
-  const { getIsMobile } = useAppInject();
+const { getIsMobile } = useAppInject();
 </script>
 
 <style scoped></style>
