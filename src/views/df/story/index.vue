059f2d3315593c4644ee07d6c65badeeea592442
gfish007 <dfish0712@outlook.com>
2024-05-14 15:52:32 +0800

    fixed

--- src/views/df/story/index.vue
+++ src/views/df/story/index.vue
@@ -629,8 +629,6 @@
     offsetHeightRef,
   );
   const { createMessage, createSuccessModal, createErrorModal, createInfoModal } = useMessage();
-  const { VITE_GLOB_APP_TITLE, VITE_GLOB_API_URL, VITE_GLOB_API_URL_PREFIX, VITE_GLOB_UPLOAD_URL } =
-    getAppEnvConfig();
 
   //故事查询
   const search = ref({
