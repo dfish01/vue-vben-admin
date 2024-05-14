cbe398a04865d9d0220ff7b285e62117cdf45970
gfish007 <dfish0712@outlook.com>
2024-05-14 10:43:58 +0800

    fixed

--- src/views/df/story/index.vue
+++ src/views/df/story/index.vue
@@ -595,6 +595,8 @@
   import { buildShortUUID } from '/@/utils/uuid';
   import { EventSourcePolyfill } from 'event-source-polyfill';
 
+  const { VITE_GLOB_APP_TITLE, VITE_GLOB_API_URL, VITE_GLOB_API_URL_PREFIX, VITE_GLOB_UPLOAD_URL } =
+    getAppEnvConfig();
   const { toClipboard } = useClipboard();
 
   const copyText = async (prompt) => {
@@ -770,7 +772,7 @@
     let sse;
     let text = '';
     uid.value = buildShortUUID('chatData');
-    const eventSource = new EventSourcePolyfill('http://localhost:7099/sse/createSse', {
+    const eventSource = new EventSourcePolyfill(VITE_GLOB_API_URL + '/sse/createSse', {
       headers: {
         Accept: 'text/event-stream',
         Authorization: token,
@@ -827,7 +829,7 @@
     let sse;
     let text = '';
     uid.value = buildShortUUID('chatData');
-    const eventSource = new EventSourcePolyfill('http://localhost:7099/sse/createSse', {
+    const eventSource = new EventSourcePolyfill(VITE_GLOB_API_URL + '/sse/createSse', {
       headers: {
         Accept: 'text/event-stream',
         Authorization: token,
